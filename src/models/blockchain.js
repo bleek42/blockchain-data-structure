935bfd91168e9accdab3ad06a26246f621724796
bleek42 <brandonleek42@gmail.com>
2021-09-18 22:01:16 -0400

    working on addTransaction, added getLastBlock in ./src/models/blockchain.js

--- src/models/blockchain.js
+++ src/models/blockchain.js
@@ -1,33 +1,40 @@
-import io from 'socket.io';
+import io from "socket.io";
 
-import { Block } from './block';
-import { Transaction } from './transaction';
-
+import { Block } from "./block";
+import { Transaction } from "./transaction";
 
 export class Blockchain {
-  constructor(blocks, io) {
-    this.block = blocks || [new Block(0, 1)];
-    this.transactions = new Set();
-    this.nodes = new Array(256);
-    this.io = io();
-  }
+	constructor(blocks, io) {
+		this.block = blocks || [new Block(0, 1, 0, [])];
+		this.transactions = [];
+		this.nodes = [];
+		this.io = io();
+	}
 
-  addNode(node) {
-    if (!node) {
-      throw TypeError('Cannot add node with a value of "null"!')
-    }
-    this.nodes.push(node);
-  }
+	addNode(node) {
+		if (!node) {
+			throw TypeError('Cannot add node with a value of "null"!');
+		}
+		this.nodes.push(node);
+	}
 
-  mineBlock(block) {
-    if (!block) {
-      throw TypeError('Cannot mine block with value of "null"!')
-    }
-    this.blocks.push(block);
-    this.io.emit(console.info('Block has been mined!'), this.toArray());
-  }
+	mineBlock(block) {
+		if (!block) {
+			throw TypeError('Cannot mine block with value of "null"!');
+		}
+		this.blocks.push(block);
+		this.io.emit(console.info("Block has been mined!"), this.toArray());
+	}
 
-  async addTransaction(transaction) {
+	lastBlock() {
+		return this.blocks(this.block.length - 1);
+	}
 
-  }
+	async addTransaction(transaction) {
+		this.transactions.push(transaction);
+		if (this.transactions.length === 2) {
+			console.info("Starting block mining...");
+			const prevBlock = this.lastBlock();
+		}
+	}
 }
